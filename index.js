a2ae40bd327d10947e376334777f574eae44f068
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-28 14:45:46 +0100

    Created GET Request to return an array of the user objects

--- index.js
+++ index.js
@@ -7,14 +7,32 @@ const db = require("./data/db");
 const server = express();
 
 server.get(cors());
-server.use(express.json())
+server.use(express.json());
 
-server.get('*', handleDefaultRequest)
+
+server.get("/api/users", getAllUsers);
+server.get("*", handleDefaultRequest);
+
+function getAllUsers(req, res) {
+  db.find()
+    .then(data => {
+      console.log(data);
+      //   res.json(data);
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        error: "The users information could not be retrieved."
+      });
+    });
+}
 
 function handleDefaultRequest(req, res) {
-    res.json('hello from afternoon project')
+  res.json("hello from afternoon project");
 }
 
 server.listen(process.env.PORT || 3800, () => {
-    console.log('listening on ' + (process.env.PORT || 3800))
-})
+  console.log("listening on " + (process.env.PORT || 3800));
+});
