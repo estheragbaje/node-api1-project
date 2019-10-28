30ad28a20efc87e72da6bacac3675526898841b6
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-28 14:20:20 +0100

    Set up a basic server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require("express");
+const cors = require("cors");
+
+const db = require("./data/db");
+
+const server = express();
+
+server.get(cors());
+server.use(express.json())
+
+server.get('*', handleDefaultRequest)
+
+function handleDefaultRequest(req, res) {
+    res.json('hello from afternoon project')
+}
+
+server.listen(process.env.PORT || 3800, () => {
+    console.log('listening on ' + (process.env.PORT || 3800))
+})
